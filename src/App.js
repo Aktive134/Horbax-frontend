97ea767b173256bfc558f52bfc0b52e34d607943
Raymond Umukoro <reiymondjon134@gmail.com>
2023-03-04 23:46:02 +0100

    initial deployment

--- src/App.js
+++ src/App.js
@@ -148,7 +148,7 @@ function App() {
             {categories.map((category) => (
               <Nav.Item key={category}>
                 <LinkContainer
-                  to={{ pathname: '/search', search: `?category=${category}` }}
+                  to={{ pathname: '/search', search: `category=${category}` }}
                   onClick={() => setSideBarIsOpen(false)}
                 >
                   <Nav.Link>{category}</Nav.Link>
